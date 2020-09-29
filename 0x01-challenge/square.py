65e7f70aa78160e29881954a99be2a4e6ba96f81
hunterxx0 <azouzimhamed@gmail.com>
2020-09-29 11:37:14 +0000

    first

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,27 +1,26 @@
 #!/usr/bin/python3
 
-class rectangle():
-    width = 0
-    height = 0
+class Square():
+    size = 0
 
 
     def __init__(self, *args, **kwargs):
         for key, value in kwargs.items():
             setattr(self, key, value)
 
-    def area_of_my_rectangle(self):
+    def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.height
+        return self.size * self.size
 
-    def PermiterOfMyRectangle(self):
-        return (self.width * 2) + (self.height * 2)
+    def PermiterOfMySquare(self):
+        return (self.size * 4)
 
     def __str__(self):
-        return "{}/{}".format(self.width, self.height)
+        return "{}".format(self.size)
 
 if __name__ == "__main__":
 
-    s = rectangle(width=12, height=9)
+    s = Square(width=12, height=9)
     print(s)
-    print(s.area_of_my_rectangle())
-    print(s.PermiterOfMyRectangle())
+    print(s.area_of_my_square())
+    print(s.PermiterOfMySquare())
